edf88ef4fdafaefc6c8fa795eb0908d2eceae34c
yyx-lhz <yin883250@gmail.com>
2026-09-13 11:45:17 +1000

    feat: 添加 Day 15 LRU 缓存题解与笔记

---
+++ day15/146-lru-cache.py
@@ -0,0 +1,56 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def remove_node(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def add_node(self, node):
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+
+        node = self.cache[key]
+        self.remove_node(node)
+        self.add_node(node)
+
+        return node.value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+            node.value = value
+
+            self.remove_node(node)
+            self.add_node(node)
+            return
+
+        node = Node(key, value)
+        self.cache[key] = node
+        self.add_node(node)
+
+        if len(self.cache) > self.capacity:
+            oldest = self.tail.prev
+            self.remove_node(oldest)
+            del self.cache[oldest.key]
