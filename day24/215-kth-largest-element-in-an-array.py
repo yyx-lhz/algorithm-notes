5c580cbdf38034a76ca9eb300ab47e9163411ba0
yuxuanyin <184073649+yyx-lhz@users.noreply.github.com>
2026-09-19 20:28:33 +0800

    docs: add Day 24 kth largest min-heap notes and solution

---
+++ day24/215-kth-largest-element-in-an-array.py
@@ -0,0 +1,13 @@
+import heapq
+
+class Solution:
+    def findKthLargest(self, nums: list[int], k: int) -> int:
+        heap = []
+
+        for num in nums:
+            heapq.heappush(heap, num)
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return heap[0]
