cec72f029b9ec2c59dbc55c9f1703990aaaf7f8c
yyx-lhz <yin883250@gmail.com>
2026-09-20 11:50:48 +0800

    docs: add Python solution for LeetCode 200

---
+++ day25/200-number-of-islands.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+
+        def dfs(r, c):
+            if (
+                r < 0 or r >= rows or
+                c < 0 or c >= cols or
+                grid[r][c] == '0'
+            ):
+                return
+
+            grid[r][c] = '0'
+            dfs(r - 1, c)
+            dfs(r + 1, c)
+            dfs(r, c - 1)
+            dfs(r, c + 1)
+
+        count = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1':
+                    count += 1
+                    dfs(r, c)
+
+        return count
+
